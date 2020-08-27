6817aab85ee9dad8b61f13b881674b03b27dd8da
Anit <codeanit@gmail.com>
2020-08-27 16:33:01 +0545

    Add string permutation example.

---
+++ algorithm/stringpermutations.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// Perm calls f with each permutation of a.
+func Perm(a []rune, f func([]rune)) {
+	perm(a, f, 0)
+}
+
+// Permute the values at index i to len(a)-1.
+func perm(a []rune, f func([]rune), i int) {
+	if i > len(a) {
+		f(a)
+		return
+	}
+	perm(a, f, i+1)
+	for j := i + 1; j < len(a); j++ {
+		a[i], a[j] = a[j], a[i]
+		perm(a, f, i+1)
+		a[i], a[j] = a[j], a[i]
+	}
+}
+func main() {
+	Perm([]rune("abc"), func(a []rune) {
+		fmt.Println(string(a))
+	})
+}
