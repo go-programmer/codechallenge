39e9874c99fba6f4f98e9832841dc1d2d2dca229
Anit <codeanit@gmail.com>
2020-08-27 16:27:58 +0545

    Solved reverse string problem.

---
+++ algorithm/reverseastring.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func reverse(str string) (result string) {
+	for _, v := range str {
+		result = string(v) + result
+	}
+	return
+}
+
+func main() {
+
+	// Reversing the string.
+	str := "Geeks"
+
+	// returns the reversed string.
+	strRev := reverse(str)
+	fmt.Println(str)
+	fmt.Println(strRev)
+}
