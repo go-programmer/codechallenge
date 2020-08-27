f333f9b59b016eea03aa193a318d0b356247f7a1
Anit <codeanit@gmail.com>
2020-08-27 16:48:09 +0545

    Add another method to reverse string using slices added.

--- algorithm/reverseastring.go
+++ algorithm/reverseastring.go
@@ -2,16 +2,29 @@ package main
 
 import "fmt"
 
-func reverse(str string) (result string) {
+func reverseUsingString(str string) (result string) {
 	for _, v := range str {
 		result = string(v) + result
 	}
 	return
 }
 
+func reverseUsiingSlices(s string) string {
+	rns := []rune(s) // convert to rune
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+
+		// swap the letters of the string,
+		// like first with last and so on.
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+
+	// return the reversed string.
+	return string(rns)
+}
+
 func main() {
-	str := ""
-	strRev := reverse(str)
+	str := "anit"
+	strRev := reverseUsiingSlices(str)
 	fmt.Println(str)
 	fmt.Println(strRev)
 }
