fba3b6ed50472b8ed818b42c1df12100d3625296
Anit <codeanit@gmail.com>
2020-08-28 17:59:40 +0545

    Add client to Codility Binary Gap client.

--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"codechallenge/codility/challenges/stockslaundering"
+	"codechallenge/codility/iterations"
 )
 
 func main() {
-	// fmt.Print(11 / 2)
+
+}
+
+func callBinaryGap() {
+	iterations.LongestBinaryGapClient()
+}
+
+func callStockLaundering() {
 	stockslaundering.Client()
 }
